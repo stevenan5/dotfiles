291ae1cee3c27761c0a6ea929366556e82ea04aa
Steven An <sla001@eng.ucsd.edu>
2025-05-20 17:00:39 -0700

    don't autocomplete when using lr snippet

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -1,7 +1,15 @@
+local config = function()
+	local npairs = require("nvim-autopairs")
+	local Rule = require("nvim-autopairs.rule")
+	local cond = require("nvim-autopairs.conds")
+	npairs.setup({ enable_check_bracket_line = false })
+	npairs.get_rule("("):with_pair(cond.not_before_text("lr"))
+	npairs.get_rule("["):with_pair(cond.not_before_text("lr"))
+	npairs.get_rule("{"):with_pair(cond.not_before_text("lr"))
+end
+
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = config,
 }
